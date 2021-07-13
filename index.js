e6ecc7b70b87e02b6dfab5821d5f0e0eb4942a4d
Abdul-commits <57894049+Abdul-commits@users.noreply.github.com>
2021-07-13 10:43:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express=require('express')
 const app=express()
 const cors=require('cors')
 const port=3002
-const {mongoose}=require('./config/database')
-const {routes}=require('./config/routes')
+const {mongoose}=require('./connection/database')
+const {routes}=require('./connection/routes')
 
 app.use(express.json())
 app.use(cors())
